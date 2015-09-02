ab613afa4d237c7ef3bd7356b44e03e96a198d38
ayumi5 <ayumi.highroof@gmail.com>
2015-09-02 18:32:38 +1200

    add complete_to_do action

---
+++ redux-to-do-list/components/ToDo.js
@@ -0,0 +1,16 @@
+import React, {findDOMNode, Component, PropTypes} from 'react';
+
+export default class ToDo extends Component {
+  render() {
+    return (
+      <li
+        onClick={this.props.onClick}
+        style={{
+          textDecoration: this.props.todo.completed ? "line-through" : "none",
+          cursor: this.props.todo.completed ? "default" : "pointer"
+        }}>
+        {this.props.todo.text}
+      </li>
+    )
+  }
+}
