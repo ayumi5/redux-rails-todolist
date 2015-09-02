e9cb838312d0b328ba7b07bac74bd630f754ff35
ayumi5 <ayumi.highroof@gmail.com>
2015-09-02 18:32:38 +1200

    add complete_to_do action

--- redux-to-do-list/actions.js
+++ redux-to-do-list/actions.js
@@ -1,5 +1,10 @@
-export const NEW_TO_DO = 'NEW_TO_DO';
+export const NEW_TO_DO = "NEW_TO_DO";
+export const COMPLETE_TO_DO = "COMPLETE_TO_DO";
 
 export function newTodo(text) {
   return {type: "NEW_TO_DO", text}
+}
+
+export function completeTodo(index) {
+  return {type: "COMPLETE_TO_DO", index}
 }
