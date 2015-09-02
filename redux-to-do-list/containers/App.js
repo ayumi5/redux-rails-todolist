c98bed9c66b34d0b04c80fa3a2fb16e387c08c1b
ayumi5 <ayumi.highroof@gmail.com>
2015-09-02 15:10:20 +1200

    remove all the changes for devtools

--- redux-to-do-list/containers/App.js
+++ redux-to-do-list/containers/App.js
@@ -1,36 +1,19 @@
 import React, {Component, PropTypes} from 'react';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 import NewToDo from '../components/new-to-do';
-import { newTodo } from '../actions';
-import newtodos from '../reducers';
-import { compose, createStore, applyMiddleware } from 'redux';
-import { devTools, persistState } from 'redux-devtools';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
-import thunk from 'redux-thunk';
+import { newTodo } from '../actions'
 
-const finalCreateStore = compose(
-  applyMiddleware(thunk),
-  devTools(),
-  persistState(
-    window.location.href.match(/[?&]debug_session=([^&]+)\b/)
-  ),
-  createStore
-);
-
-const store = finalCreateStore(newtodos);
-
-export default class App extends Component {
+class App extends Component {
   render(){
     const { dispatch } = this.props;
     return (
       <div>
-        <Provider store={store}>
-          {() => <NewToDo onAddClick={text => dispatch(newTodo(text))}/>}
-        </Provider>
-        <DebugPanel top right bottom>
-          <DevTools store={store} monitor={LogMonitor} />
-        </DebugPanel>
+        <NewToDo onAddClick={text =>
+          dispatch(newTodo(text))
+        }/>
       </div>
     );
   }
 }
+
+export default connect()(App);
