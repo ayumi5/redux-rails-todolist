4a817b7f8ac7e219e9452f68dafa1d4258986c79
ayumi5 <ayumi.highroof@gmail.com>
2015-09-07 16:21:46 +1200

    improve styling a bit

--- rails-to-do-list/app/controllers/listings_controller.rb
+++ rails-to-do-list/app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController
 
   def index
-    @todos = {'todo' => 'Pat a dog', 'completed' => true}
+    @todos = [{'todo' => 'Pat a dog', 'completed' => true}, {'todo' => 'Play with a dog', 'completed' => false}]
 
     respond_to do |format|
       format.html
