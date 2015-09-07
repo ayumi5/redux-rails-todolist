2c243447850a3fa8be4ddfc4f77f0450e7332070
ayumi5 <ayumi.highroof@gmail.com>
2015-09-07 18:35:44 +1200

    post data using xhr

--- rails-to-do-list/app/controllers/listings_controller.rb
+++ rails-to-do-list/app/controllers/listings_controller.rb
@@ -1,11 +1,15 @@
 class ListingsController < ApplicationController
+  protect_from_forgery except: [:create]
 
   def index
     @todos = [{'todo' => 'Pat a dog', 'completed' => true}, {'todo' => 'Play with a dog', 'completed' => false}]
-
     respond_to do |format|
       format.html
       format.json { render json: @todos }
     end
+  end
+
+  def create
+    redirect_to action: 'index', status: :ok
   end
 end
